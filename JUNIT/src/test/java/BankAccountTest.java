ee1ccad3c1a0f257d3e081a93294300956c51a3e
Noel Guillen <noelblas274@gmail.com>
2024-07-18 20:55:49 -0400

    test name excerise added

--- JUNIT/src/test/java/BankAccountTest.java
+++ JUNIT/src/test/java/BankAccountTest.java
@@ -1,10 +1,13 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Test BankAccount Class")
 public class BankAccountTest {
 
     @Test
+    @DisplayName("Withdraw 500 successfully")
     public void testWithdraw(){
         BankAccount bankAccount = new BankAccount(500, -1000);
 
@@ -14,6 +17,7 @@ public class BankAccountTest {
     }
 
     @Test
+    @DisplayName("Deposit 400 successfully")
     public void testDepost(){
         BankAccount bankAccount = new BankAccount(400, 0);
 
