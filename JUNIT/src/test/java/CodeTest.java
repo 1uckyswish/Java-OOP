1418a8291d15487c3de5ee7bbc1fe1904b4fdfd2
Noel Guillen <noelblas274@gmail.com>
2024-07-18 20:36:11 -0400

    starter unit test from linkedin

---
+++ JUNIT/src/test/java/CodeTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class CodeTest {
+
+    @Test
+    public void testSayHello(){
+        Code code = new Code();
+        assertEquals("Hello World", code.sayHello());
+    }
+}
---
+++ JUNIT/src/test/java/TestTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class TestTest {
+
+    @Test
+    public void test(){
+
+    }
+}
